27f285b05348ee12b64c8f317ef262bd3346f7fe
whath <danigenov@protonmail.com>
2026-07-14 14:37:11 +0200

    feat: add books display

--- app/books/page.tsx
+++ app/books/page.tsx
@@ -1,7 +1,17 @@
-export default function BooksPage() {
+export default async function BooksPage() {
+  const booksAPIResponse = await fetch("http://localhost:3000/api/books");
+  const books = await booksAPIResponse.json(); //array of book objects
   return (
     <main>
       <h1>Todos los libros</h1>
+
+      <ul>
+        {books.map((book: { title: string; id: number }) => (
+          <li key={book.id}>
+            Libro {book.title} con ID {book.id}
+          </li>
+        ))}
+      </ul>
     </main>
   );
 }
