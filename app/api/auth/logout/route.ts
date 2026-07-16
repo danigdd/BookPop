67512be170de3f9e1c7eea743aedda2ac67e20bb
whath <danigenov@protonmail.com>
2026-07-17 01:07:10 +0200

    feat : add jwt authentication and protected routes

--- app/api/auth/logout/route.ts
+++ app/api/auth/logout/route.ts
@@ -1,20 +1,14 @@
 import { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
 import { getCookieValue } from "@/utils/cookies";
-import { deleteSessionUser } from "@/services/sessionsService";
 
 export async function DELETE(request: NextRequest) {
   try {
-    const cookieHeader = request.headers.get("Cookie");
-    const tokenId = getCookieValue(cookieHeader, "auth_token");
-
-    await deleteSessionUser(tokenId);
-
     const response = NextResponse.json({
       message: "Logout succesful",
     });
 
-    response.cookies.delete("auth_token");
+    response.cookies.delete("jwt_token");
     return response;
   } catch (error) {
     return Response.json({ message: "Something went wrong" }, { status: 500 });
